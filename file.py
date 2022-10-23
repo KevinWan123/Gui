faad678331f8c88370c026a7eb65fb66da267200
Kevin Wan <114878518+KevinWan123@users.noreply.github.com>
2022-10-23 18:00:32 -0400

    Add files via upload

---
+++ file.py
@@ -0,0 +1,33 @@
+from msilib.schema import File
+from pydoc import cli
+from tkinter import *
+from PIL import Image
+
+
+root = Tk()
+root.title('myTilte')
+root.geometry("600x400")
+
+my_menu = Menu(root)
+
+root.config(menu=my_menu)
+#click command
+def our_command():
+    pass
+#Create a menu item
+
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu) #File option
+
+file_menu.add_command(label="New...", command=our_command)
+file_menu.add_separator()#Creates bar
+file_menu.add_cascade(label="Exit..",command=our_command)
+
+
+#create and edit menu item.
+
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label='Settings', menu = edit_menu)
+edit_menu.add_command(label='Settings1', command=our_command)
+edit_menu.add_command(label='Settings2', command=root.quit)
+root.mainloop()
